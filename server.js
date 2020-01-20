e0c9cd52ab8a65baf0ce8f7827bcace0bf01b3e4
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2020-01-20 10:01:13 -0300

    Modulo mensaje comenzado

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const router = express.Router()
-const response = require('./network/response')
+
+const router = require('./network/routes')
 
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(router)
 
-router.get('/message', (req, res) => {
-  console.log(req.headers)
-  res.header({
-    'custom-header' : 'Nuestro valor personalizado'
-  })
-  response.success(req, res, 'Lista de mensajes')
-})
+router(app)
 
-router.post('/message', (req, res) => {
-  console.log(req.query)
-  if (req.query.error == 'ok'){
-    response.error(req, res, 'Error simulado', 400)
-  }else{
-    response.success(req, res, 'Creado correctamente', 201)
-  }
-})
+app.use('/app', express.static('public'))
 
 app.listen(3000)
 
