edea957bae942290d38155481c7f1fd19a8f3042
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2020-01-20 08:52:21 -0300

    Router

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 var app = express()
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router)
 
 router.get('/', (req, res) => {
-  res.send('Hola desde GET')
+  res.send('Hola desde GET nuevo')
 })
 
 router.post('/', (req, res) => {
+  console.log(req.query)
+  //?orderBy=id&age=25
+  console.log(req.body)
   res.send('Hola desde POST')
 })
 
